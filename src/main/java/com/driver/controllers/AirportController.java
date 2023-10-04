114e23a17ab2f7c5a58424d90ea2ddd9d21df39d
Dipak Rekhate <drekhate@gmail.com>
2023-10-04 19:33:17 +0530

    four

--- src/main/java/com/driver/controllers/AirportController.java
+++ src/main/java/com/driver/controllers/AirportController.java
@@ -1,84 +1,55 @@
 package com.driver.controllers;
 
 
-import com.driver.Service.AirportService;
+import com.driver.service.TripService;
 import com.driver.model.Airport;
 import com.driver.model.City;
 import com.driver.model.Flight;
 import com.driver.model.Passenger;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.Date;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Objects;
 
 @RestController
 public class AirportController {
-//    @Autowired
-//    AirportService airportService;
-    AirportService airportService = new AirportService();
+    private TripService tripService=new TripService();
     @PostMapping("/add_airport")
     public String addAirport(@RequestBody Airport airport){
 
         //Simply add airport details to your database
         //Return a String message "SUCCESS"
-        airportService.addAirport(airport);
+        tripService.addAirPort(airport);
         return "SUCCESS";
     }
 
-    @GetMapping("/get-largest-aiport")
-    public String getLargestAirportName(){
+    @PostMapping("/add-passenger")
+    public String addPassenger(@RequestBody Passenger passenger)
+    {
 
-        //Largest airport is in terms of terminals. 3 terminal airport is larger than 2 terminal airport
-        //Incase of a tie return the Lexicographically smallest airportName
+        //Add a passenger to the database
+        //And return a "SUCCESS" message if the passenger has been added successfully.
 
-       return airportService.getLargestAirportName();
+        return tripService.addPassenger(passenger);
     }
 
-    @GetMapping("/get-shortest-time-travel-between-cities")
-    public double getShortestDurationOfPossibleBetweenTwoCities(@RequestParam("fromCity") City fromCity, @RequestParam("toCity")City toCity){
+    @PostMapping("/add-flight")
+    public String addFlight(@RequestBody Flight flight)
+    {
 
-        //Find the duration by finding the shortest flight that connects these 2 cities directly
-        //If there is no direct flight between 2 cities return -1.
-
-       return airportService.getShortestDurationOfPossibleBetweenTwoCities(fromCity, toCity);
+        //Return a "SUCCESS" message string after adding a flight.
+        return tripService.addFlight(flight);
     }
-
-    @GetMapping("/get-number-of-people-on-airport-on/{date}")
-    public int getNumberOfPeopleOn(@PathVariable("date") Date date,@RequestParam("airportName")String airportName){
-
-        //Calculate the total number of people who have flights on that day on a particular airport
-        //This includes both the people who have come for a flight and who have landed on an airport after their flight
-
-        return airportService.getNumberOfPeopleOn(date, airportName);
-    }
-
-    @GetMapping("/calculate-fare")
-    public int calculateFlightFare(@RequestParam("flightId")Integer flightId){
-
-        //Calculation of flight prices is a function of number of people who have booked the flight already.
-        //Price for any flight will be : 3000 + noOfPeopleWhoHaveAlreadyBooked*50
-        //Suppose if 2 people have booked the flight already : the price of flight for the third person will be 3000 + 2*50 = 3100
-        //This will not include the current person who is trying to book, he might also be just checking price
-
-       return 0;
-
-    }
-
 
     @PostMapping("/book-a-ticket")
-    public String bookATicket(@RequestParam("flightId")Integer flightId,@RequestParam("passengerId")Integer passengerId){
+    public String bookATicket(@RequestParam("flightId")Integer flightId,@RequestParam("passengerId")Integer passengerId)
+    {
 
         //If the numberOfPassengers who have booked the flight is greater than : maxCapacity, in that case :
         //return a String "FAILURE"
         //Also if the passenger has already booked a flight then also return "FAILURE".
         //else if you are able to book a ticket then return "SUCCESS"
 
-        return null;
+        return tripService.bookATicket(flightId,passengerId);
     }
 
     @PutMapping("/cancel-a-ticket")
@@ -89,23 +60,56 @@ public class AirportController {
         // Otherwise return a "SUCCESS" message
         // and also cancel the ticket that passenger had booked earlier on the given flightId
 
-       return null;
+        return tripService.cancelATicket(flightId,passengerId);
     }
+
+    @GetMapping("/get-largest-aiport")
+    public String getLargestAirportName(){
+
+        //Largest airport is in terms of terminals. 3 terminal airport is larger than 2 terminal airport
+        //Incase of a tie return the Lexicographically smallest airportName
+
+        return tripService.getLargestAirPort();
+    }
+
+    @GetMapping("/get-shortest-time-travel-between-cities")
+    public double getShortestDurationOfPossibleBetweenTwoCities(@RequestParam("fromCity") City fromCity, @RequestParam("toCity")City toCity){
+
+        //Find the duration by finding the shortest flight that connects these 2 cities directly
+        //If there is no direct flight between 2 cities return -1.
+
+        return tripService.getShortestDurationOfPossibleBetweenTwoCities(fromCity,toCity);
+    }
+
+    @GetMapping("/get-number-of-people-on-airport-on/{date}")
+    public int getNumberOfPeopleOn(@PathVariable("date") Date date,@RequestParam("airportName")String airportName)
+    {
+
+        //Calculate the total number of people who have flights on that day on a particular airport
+        //This includes both the people who have come for a flight and who have landed on an airport after their flight
+
+        return tripService.getNumberOfPeopleOn(date,airportName);
+    }
+
+    @GetMapping("/calculate-fare")
+    public int calculateFlightFare(@RequestParam("flightId")Integer flightId){
+
+        //Calculation of flight prices is a function of number of people who have booked the flight already.
+        //Price for any flight will be : 3000 + noOfPeopleWhoHaveAlreadyBooked*50
+        //Suppose if 2 people have booked the flight already : the price of flight for the third person will be 3000 + 2*50 = 3100
+        //This will not include the current person who is trying to book, he might also be just checking price
+
+        return tripService.calculateFare(flightId);
+
+    }
+
 
 
     @GetMapping("/get-count-of-bookings-done-by-a-passenger/{passengerId}")
     public int countOfBookingsDoneByPassengerAllCombined(@PathVariable("passengerId")Integer passengerId){
 
         //Tell the count of flight bookings done by a passenger: This will tell the total count of flight bookings done by a passenger :
-       return 0;
-    }
-
-    @PostMapping("/add-flight")
-    public String addFlight(@RequestBody Flight flight){
-
-        //Return a "SUCCESS" message string after adding a flight.
-        airportService.addFlight(flight);
-       return "SUCCESS";
+        return tripService.countOfBookingsDoneByPassengerAllCombined(passengerId);
     }
 
 
@@ -115,7 +119,8 @@ public class AirportController {
         //We need to get the starting airportName from where the flight will be taking off (Hint think of City variable if that can be of some use)
         //return null incase the flightId is invalid or you are not able to find the airportName
 
-        return null;
+
+        return tripService.getAirportNameFromFlightId(flightId);
     }
 
 
@@ -127,18 +132,7 @@ public class AirportController {
         //Revenue will also decrease if some passenger cancels the flight
 
 
-        return 0;
+
+        return tripService.calculateRevenueOfAFlight(flightId);
     }
-
-
-    @PostMapping("/add-passenger")
-    public String addPassenger(@RequestBody Passenger passenger){
-
-        //Add a passenger to the database
-        //And return a "SUCCESS" message if the passenger has been added successfully.
-
-       return null;
-    }
-
-
 }
